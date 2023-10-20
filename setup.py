8d159c2548a1c16a908b702ecbdf2571404e5753
Lukas Trippe <lkstrp@pm.me>
2023-10-20 14:45:26 +0200

    Changed package name rise->iea_rise

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="rise",
+    name="iea_rise",
     version="0.1.0",
     author="Lukas Trippe",
     author_email="lkstrp@pm.me",
